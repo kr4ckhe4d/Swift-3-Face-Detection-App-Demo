01f8c2d1c5181199339c2264222089b773871658
Nipuna <nipuna@app-monkeyz.com>
2016-11-15 16:08:54 +0530

    Initial commit

---
+++ Detector/CameraViewController.swift
@@ -0,0 +1,24 @@
+
+
+//
+//  CameraViewController.swift
+//  Detector
+//
+//  Created by Gregg Mojica on 8/22/16.
+//  Copyright © 2016 Gregg Mojica. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func takePhoto(sender: AnyObject) {
+    }
+ 
+    
+   }
--- Detector/ViewController.swift
+++ Detector/ViewController.swift
@@ -7,19 +7,16 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var personPic: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        personPic.image = UIImage(named: "face-1")
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
-
